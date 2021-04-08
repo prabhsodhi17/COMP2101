8c004000e5c3084614d18fd6a4aa3e56c17ad0e4
prabhsodhi17 <78510276+prabhsodhi17@users.noreply.github.com>
2021-04-08 14:09:11 -0400

    Create IPConfigReport.ps1

--- Powershell/IPConfigReport.ps1
+++ Powershell/IPConfigReport.ps1
@@ -1,6 +1,4 @@
-get-ciminstance win32_networkadapterconfiguration |
-Where-Object IPEnabled -eq $True |
-Select-Object Description, Index, IPAddress,
+get-ciminstance win32_networkadapterconfiguration | Where-Object IPEnabled -eq $True | Select-Object Description, Index, IPAddress,
 @{n="Subnet Mask";e={$_.IPSubnet}},
 @{n="DNS Domain Name";e={$_.DNSHostName}},
 @{n="DNS Server";e={$_.DNSServerSearchOrder}} |
